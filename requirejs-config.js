4d6178e063b4306bed9699d386d5b9c497755103
chubin1999 <phachgia1999@gmail.com>
2021-03-03 10:47:24 +0700

    phuc

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,10 +1,18 @@
 var config = {
 	paths: {            
 		fontawesome: "js/fontawesome",
+		'owlcarousel': "js/owl.carousel",
+		slick: 'js/slick'
 	},   
 	shim: {
 		'fontawesome': {
 			deps: ['jquery']
-		}
-	}
+		},
+		'owlcarousel': {
+			deps: ['jquery']
+		},
+		slick: {
+            deps: ['jquery']
+        }
+	},
 };
